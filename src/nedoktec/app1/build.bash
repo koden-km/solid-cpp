1f95504800b0f3870484dbbb7d5149ffd31eb8e3
Kevin Millar <koden.km@gmail.com>
2013-03-08 00:05:26 +1000

    Trying to setup a build system.

---
+++ src/nedoktec/app1/build.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Temporary build script until i can get Boost.Build working.
+g++-mp-4.7 -o app1.bin -std=c++11 *.cpp
---
+++ src/nedoktec/app1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../solid/solid.hpp"
+
+int main(int argc, const char* argv[])
+{
+    std::cout << "Solid v" << NEDOKTEC_SOLID_VERSION << std::endl;
+    return 0;
+}
